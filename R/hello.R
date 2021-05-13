5296ec9d0495c3bc667576b1aec3051174c4bdd6
elijade <elija.dentler@web.de>
2021-05-13 15:30:14 +0200

    update

---
+++ R/hello.R
@@ -0,0 +1,17 @@
+
+
+#' My Hello World Function
+#' @param x the name of the person to say hi to
+#' @return The output from \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("John")
+#' \dontrun{
+#' hello("Steve")
+#' }
+hello <- function(x) {
+  print(paste("Hello ", x,"this is the world!"))
+}
+
+
