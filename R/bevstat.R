b7112852e906c737b7b6471b80e1ae469185c0bb
elijade <elija.dentler@web.de>
2021-05-18 12:47:23 +0200

    update

--- R/bevstat.R
+++ R/bevstat.R
@@ -140,6 +140,7 @@ read_bevstat <- function() {
 #' @return two tables, one for age distribution and one for mean age
 #' @importFrom plyr ddply
 #' @importFrom scales label_percent
+#' @import dplyer
 #' @export
 calculate_age <- function() {
 
